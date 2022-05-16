50d6065a1e626391b2d661df1a3a25d5b19b041e
sharmisthachoudhuri <sharmistha.choudhuri193@gmail.com>
2022-05-16 15:38:21 +0530

    Refactored code to have all output in single excel file

--- src/main/java/org/datavalidator/constants/ApplicationConstants.java
+++ src/main/java/org/datavalidator/constants/ApplicationConstants.java
@@ -1,9 +1,17 @@
 package org.datavalidator.constants;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ApplicationConstants
 {
     public static final String REPORT_OUTPUT_FILE_LOCATION = "./report-output.xlsx";
     public static final String DATA_MISMATCH_SHEET_NAME = "Data Mismatch Report";
     public static final String DATA_DUPLICATION_SHEET_NAME = "Data Duplication Report";
     public static final String DATA_DUPLICATION_SHEET_COLUMN_NAME = "Source Row Number With Duplicates";
+
+    static{
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        System.setProperty("current.date", dateFormat.format(new Date()));
+    }
 }
